b2c0b6d6e319105d6ede214e150fddebde5ae9c2
ValitovGaziz <valitovgaziz@gmail.com>
2022-10-24 19:51:25 +0500

    Looking for loop in node trees.

---
+++ src/LoopInspector.java
@@ -0,0 +1,25 @@
+import org.w3c.dom.Node;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class LoopInspector {
+    public static void main(String[] args) {
+
+    }
+
+    public static int loopSize(Node node) {
+        HashMap<Node, Integer> map = new HashMap<>();
+        int index = 1;
+        map.put(node, index++);
+        Node next;
+        while (true) {
+            next = node.getNext();
+            if (map.keySet().contains(next)) {
+                return map.get(node) - map.get(next) + 1;
+            }
+            map.put(next, index++);
+            node = next;
+        }
+    }
+}
