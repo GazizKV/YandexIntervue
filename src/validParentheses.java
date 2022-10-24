69c4ca93da09e8c2f493d6f9091be5482f2f9dc4
ValitovGaziz <valitovgaziz@gmail.com>
2022-10-24 19:31:53 +0500

    Counting valid parentheses.

---
+++ src/validParentheses.java
@@ -0,0 +1,17 @@
+import java.util.Queue;
+
+public class validParentheses {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean validParentheses(String parentheses) {
+        int result = 0;
+        for (String next : parentheses.split("")) {
+            if (next.equals("(")) result++;
+            else if (next.equals(")")) result--;
+            if (result < 0) return false;
+            return result == 0;
+        }
+    }
+}
